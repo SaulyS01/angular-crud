c454a08cdba7bf829530acc1912071c6e753bfcd
saulyS01 <saul.ytucayasi@upeu.edu.pe>
2021-10-19 09:52:41 -0500

    CRUD
    crud-categorias

---
+++ src/app/components/tutorial-details/tutorial-details.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit, Input, OnChanges, Output, EventEmitter } from '@angular/core';
+import Tutorial from 'src/app/models/tutorial.model';
+import { TutorialService } from 'src/app/services/tutorial.service';
+
+@Component({
+  selector: 'app-tutorial-details',
+  templateUrl: './tutorial-details.component.html',
+  styleUrls: ['./tutorial-details.component.css']
+})
+export class TutorialDetailsComponent implements OnInit, OnChanges {
+
+  @Input() tutorial?: Tutorial;
+  @Output() refreshList: EventEmitter<any> = new EventEmitter();
+  currentTutorial: Tutorial = {
+    title: '',
+    description: '',
+  };
+  message = '';
+
+  constructor(private tutorialService: TutorialService) { }
+
+  ngOnInit(): void {
+    this.message = '';
+  }
+
+  ngOnChanges(): void {
+    this.message = '';
+    this.currentTutorial = { ...this.tutorial };
+  }
+
+  updateState(status: boolean): void {
+    if (this.currentTutorial.key) {
+      this.tutorialService.update(this.currentTutorial.key, { state: status })
+      .then(() => {
+        this.currentTutorial.state = status;
+        this.message = 'El estado se actualizó correctamente!';
+      })
+      .catch(err => console.log(err));
+    }
+  }
+
+  updateTutorial(): void {
+    const data = {
+      title: this.currentTutorial.title,
+      description: this.currentTutorial.description
+    };
+
+    if (this.currentTutorial.key) {
+      this.tutorialService.update(this.currentTutorial.key, data)
+        .then(() => this.message = 'Se ha actualizado correctamente!')
+        .catch(err => console.log(err));
+    }
+  }
+
+  deleteTutorial(): void {
+    if (this.currentTutorial.key) {
+      this.tutorialService.delete(this.currentTutorial.key)
+        .then(() => {
+          this.refreshList.emit();
+          this.message = 'Se ha eliminado correctamente!';
+        })
+        .catch(err => console.log(err));
+    }
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA9s_z8ttpAVoaD1GUQYhnQ8D8uVa4aCWg",
+    authDomain: "catalogo-a7e6d.firebaseapp.com",
+    databaseURL: "https://catalogo-a7e6d-default-rtdb.firebaseio.com",
+    projectId: "catalogo-a7e6d",
+    storageBucket: "catalogo-a7e6d.appspot.com",
+    messagingSenderId: "574931088573",
+    appId: "1:574931088573:web:ca6037b65dad1b93ac33d4",
+    measurementId: "G-XY8RMKG7MF"
+  }
+};
